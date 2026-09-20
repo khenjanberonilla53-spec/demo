7b56ba22f1f46381bfc9c954ab6d7302720c936d
khenjanberonilla53 <khenjanberonilla53@gmail.com>
2026-09-20 19:33:24 +0800

    First Commit

---
+++ beronilla_demo.js
@@ -0,0 +1,12 @@
+let name = "Victor Magtanggol";
+let age = 21;
+let birthday = "July 1, 2004";
+let address = "Japan";
+let hobby = "Playing games";
+
+console.log("BIODATA");
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Birthday: " + birthday);
+console.log("Address: " + address);
+console.log("Hobby: " + hobby);
